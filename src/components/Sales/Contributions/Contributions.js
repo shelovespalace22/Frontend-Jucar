139f0e69a52b9fa987e4872cafcf657436543ef3
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2024-03-18 13:53:30 -0500

    Creación componente OrderDetails

--- src/components/Sales/Contributions/Contributions.js
+++ src/components/Sales/Contributions/Contributions.js
@@ -6,17 +6,16 @@ import { faPlus, faEdit, faTrash, faEye } from '@fortawesome/free-solid-svg-icon
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2';
-const Contributions = ({orderId}) => {
-    
-  
-    const currentDate = new Date();
-    const formattedDate = currentDate.toISOString().split('T')[0];
-    const [contributions, setContributions] = useState([]);
-    const [newContribution, setNewContribution] = useState ({
-        PaymentMethodId: '',
-        AmountPaid: 0,
-        ContributionDate: formattedDate
-    })
+
+const Contributions = ({ orderId }) => {
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split('T')[0];
+  const [contributions, setContributions] = useState([]);
+  const [newContribution, setNewContribution] = useState ({
+    PaymentMethodId: '',
+    AmountPaid: 0,
+    ContributionDate: formattedDate
+  });
   const [showModal, setShowModal] = useState(false);
   const [modalAction, setModalAction] = useState('create');
   const [selectedContributionId, setSelectedContributionId] = useState('');
@@ -29,33 +28,36 @@ const Contributions = ({orderId}) => {
 
   useEffect(()=>{
     const fetchContributions = async () =>{
-        try{
-            const response = axios.get(`https://localhost:7028/api/orders/${orderId}/contributions`);
-            setContributions(response.data);
+      try{
+        console.log(contributions);
+        const response = await axios.get(`https://localhost:7028/api/orders/${orderId}/contributions`);
 
-        }catch (error){
-            console.error ('error fetching contribution', error);
+        setContributions(response.data);
 
-        }
+      } catch (error){
+        console.error ('error fetching contribution', error);
+      }
     };
+
     fetchContributions();
-  },[orderId]);
+
+  }, [orderId]);
 
   const handleCreateContribution = async () => {
     try {
       const response = await axios.post(`https://localhost:7028/api/orders/${orderId}/contributions`, newContribution);
-  
+
       setContributions([response.data, ...contributions]);
-  
+
       setNewContribution({
         PaymentMethodId: '',
         AmountPaid: 0,
         ContributionDate: formattedDate
       });
-  
+
       handleCloseModal();
-  
-    
+
+
       Swal.fire(
         '¡Éxito!',
         '¡La contribución ha sido creada exitosamente.',
@@ -63,8 +65,7 @@ const Contributions = ({orderId}) => {
       );
     } catch (error) {
       console.error('Error creating contribution', error);
-  
-      
+
       Swal.fire(
         'Error',
         'Hubo un problema al crear la contribución.',
@@ -72,7 +73,6 @@ const Contributions = ({orderId}) => {
       );
     }
   };
-  
 
   const handleUpdateContribution = async () => {
     try {
@@ -80,21 +80,21 @@ const Contributions = ({orderId}) => {
         `https://localhost:7028/api/orders/${orderId}/contributions/${selectedContributionId}`,
         newContribution
       );
-  
+
       const response = await axios.get(`https://localhost:7028/api/orders/${orderId}/contributions`);
-  
+
       const updatedContributions = response.data;
-  
+
       setContributions(updatedContributions);
-  
+
       setNewContribution({
         PaymentMethodId: '',
         AmountPaid: 0,
         ContributionDate: formattedDate
       });
-  
+
       handleCloseModal();
-  
+
       Swal.fire(
         '¡Éxito!',
         '¡La contribución ha sido actualizada exitosamente.',
@@ -102,7 +102,7 @@ const Contributions = ({orderId}) => {
       );
     } catch (error) {
       console.error('Error updating contribution:', error);
-  
+
       Swal.fire(
         'Error',
         'Hubo un problema al actualizar la contribución.',
@@ -110,29 +110,28 @@ const Contributions = ({orderId}) => {
       );
     }
   };
-  
 
   const handleDeleteContribution = async (contributionId) => {
-    
+
     Swal.fire({
-      title: '¿Estás seguro?',
-      text: '¡No podrás revertir esto!',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Sí, eliminarlo!'
+    title: '¿Estás seguro?',
+    text: '¡No podrás revertir esto!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, eliminarlo!'
     }).then(async (result) => {
-        
+
       if (result.isConfirmed) {
         try {
-         
+
           await axios.delete(`https://localhost:7028/api/orders/${orderId}/contributions/${contributionId}`);
 
           const updatedContributions = contributions.filter((contribution) => contribution.contributionID !== contributionId);
 
           setContributions(updatedContributions);
-          
+
           Swal.fire(
             '¡Eliminado!',
             '¡Tu contribución ha sido eliminada.',
@@ -140,7 +139,7 @@ const Contributions = ({orderId}) => {
           );
         } catch (error) {
           console.error('Error deleting contribution:', error);
-          
+
           Swal.fire(
             'Error',
             'Hubo un problema al eliminar la contribución.',
@@ -150,7 +149,6 @@ const Contributions = ({orderId}) => {
       }
     });
   };
-  
 
   const handleShowCreateModal = () => {
     setModalAction('create');
@@ -161,32 +159,31 @@ const Contributions = ({orderId}) => {
     setModalAction ('edit');
     setSelectedContributionId(contributionId);
 
-    const selectedContribution = contributions.find ((contribution)=> contribution.contributionID === contributionId);
+    const selectedContribution = contributions.find((contribution) => contribution.contributionID === contributionId);
 
     if (selectedContribution){
-        setNewContribution({
-            PaymentMethodId: selectedContribution.paymentMethodId ||  '',
-            AmountPaid:selectedContribution.amountPaid || 0,
-            ContributionDate:selectedContribution.contributionDate|| formattedDate
-        })
+      setNewContribution({
+        PaymentMethodId: selectedContribution.paymentMethodId ||  '',
+        AmountPaid:selectedContribution.amountPaid || 0,
+        ContributionDate:selectedContribution.contributionDate|| formattedDate
+      });
     }
 
     setShowModal(true);
   };
 
-
   const handleShowDetailModal = (contributionId) => {
-    setModalAction ('Detail');
+    setModalAction ('detail');
     setSelectedContributionId(contributionId);
 
-    const selectedContribution = contributions.find ((contribution)=> contribution.contributionID === contributionId);
+    const selectedContribution = contributions.find((contribution) => contribution.contributionID === contributionId);
 
     if (selectedContribution){
-        setNewContribution({
-            PaymentMethodId: selectedContribution.paymentMethodId ||  '',
-            AmountPaid:selectedContribution.amountPaid || 0,
-            ContributionDate:selectedContribution.contributionDate|| formattedDate
-        })
+      setNewContribution({
+        PaymentMethodId: selectedContribution.paymentMethodId ||  '',
+        AmountPaid: selectedContribution.amountPaid || 0,
+        ContributionDate: selectedContribution.contributionDate|| formattedDate
+      });
     }
 
     setShowModal(true);
@@ -194,11 +191,13 @@ const Contributions = ({orderId}) => {
 
   const handleCloseModal = () => {
     setShowModal(false);
+
     setNewContribution({
-        PaymentMethodId: '',
-        AmountPaid: 0,
-        ContributionDate: formattedDate
+      PaymentMethodId: '',
+      AmountPaid: 0,
+      ContributionDate: formattedDate
     });
+
     setSelectedContributionId('');
   };
 
@@ -206,59 +205,63 @@ const Contributions = ({orderId}) => {
 
   const handleGoBack = () => {
     history.goBack();
-};
+  };
 
-return(
+  return(
     <div className='contributions-container'>
-        {/* <header>
-            <div>
-                <img src = '' alt='logo'/>
-                <h1>AutopartesJUCAR</h1>
-            </div>
-        </header> */}
-        <br>
-        <h2>Contribuciones</h2>
-        </br>
-        <Button variant="primary" onClick={handleShowCreateModal}>
+
+      <br/>
+      <h2>Contribuciones</h2>
+      <br/>
+
+      <Button variant="primary" onClick={handleShowCreateModal}>
         <FontAwesomeIcon icon={faPlus} /> Nueva Contribucion
-        </Button>
-        <Button variant="danger" onClick={handleGoBack}>
-            Volver
-        </Button>
+      </Button>
 
-        <hr/>
-        <Table>
-            <thead>
-                <tr>
-                    <th>Metodo de pago</th>
-                    <th>Cantidad pagada</th>
-                    <th>Fecha de la contribucion</th>
-                </tr>
-            </thead>
-            <tbody>
-                {currentContributions.map ((contribution) => (
-                    <tr key={contribution.contributionId}>
-                        <td>{contribution.paymentMethodId}</td>
-                        <td>{contribution.amountPaid}</td>
-                        <td>{contribution.contributionDate}</td>
-                        <td>
-                        <Button variant="info" onClick={() => handleShowEditModal(contribution.contributionID)}>
-                        <FontAwesomeIcon icon={faEdit} /> Actualizar
-                        </Button>
-                        <Button variant="danger" onClick={() => handleDeleteContribution(contribution.contributionID)}>
-                        <FontAwesomeIcon icon={faTrash} /> Eliminar
-                        </Button>
-                        <Button variant="primary" onClick={() => handleShowDetailModal(contribution.contributionID)}>
-                        <FontAwesomeIcon icon={faEye} /> Ver Detalle
-                        </Button>
-                        </td>
+      <Button variant="danger" onClick={handleGoBack}>
+        Volver
+      </Button>
 
-                    </tr>
-                ))}
-            </tbody>
-        </Table>
+      <hr/>
 
-        <Pagination>
+      <Table>
+        <thead>
+          <tr>
+            <th>Método de Pago</th>
+            <th>Cantidad Pagada</th>
+            <th>Fecha del Pago</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {currentContributions.map((contribution) => (
+            <tr key={contribution.contributionID}>
+              <td>{contribution.paymentMethodId}</td>
+              <td>{contribution.amountPaid}</td>
+              <td>{contribution.contributionDate}</td>
+              <td>
+
+                <Button variant="info" onClick={() => handleShowEditModal(contribution.contributionID)}>
+                  <FontAwesomeIcon icon={faEdit} /> Actualizar
+                </Button>
+
+                <Button variant="danger" onClick={() => handleDeleteContribution(contribution.contributionID)}>
+                  <FontAwesomeIcon icon={faTrash} /> Eliminar
+                </Button>
+
+                <Button variant="primary" onClick={() => handleShowDetailModal(contribution.contributionID)}>
+                  <FontAwesomeIcon icon={faEye} /> Ver Detalle
+                </Button>
+
+              </td>
+
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+
+      <Pagination>
         {Array.from({ length: Math.ceil(contributions.length / itemsPerPage) }, (_, index) => (
           <Pagination.Item key={index + 1} active={index + 1 === currentPage} onClick={() => paginate(index + 1)}>
             {index + 1}
@@ -267,92 +270,90 @@ return(
       </Pagination>
 
       <Modal show ={showModal} onHide={handleCloseModal}>
+
         <Modal.Header closeButton>
-            <Modal.Title>
-                {modalAction === 'create'
-                ? 'nueva contribucion'
-                :modalAction === 'edit'
-                ? 'Actualizar contribucion'
-                : 'Detalle de la contribucion'}
-            </Modal.Title>
+
+          <Modal.Title>
+            {modalAction === 'create'
+            ? 'Nueva Contribución'
+            :modalAction === 'edit'
+            ? 'Actualizar Contribución'
+            : 'Detalles de Contribución'}
+          </Modal.Title>
 
         </Modal.Header>
 
         <Modal.Body>
-            {modalAction !== 'detail' &&(
-                <Form>
-                    <Form.Group controlId='formMethodId'>
-                        <Form.Label>Id de metodo de pago</Form.Label>
-                        <Form.Control
-                         type= "text"
-                         placeholder='Ingrese el id del metodo de pago que se uso'
-                         value={newContribution.PaymentMethodId}
-                         onChange={(e)=> setNewContribution({...newContribution, PaymentMethodId: e.target.value})}
-                        
-                        />
-                         
-                    </Form.Group>
-                    <Form.Group controlId='formAmountPaid'>
-                        <Form.Label>Cantidad pagada</Form.Label>
-                        <Form.Control
-                         type= "number"
-                         placeholder='Ingrese la cantidad pagada'
-                         value={newContribution.AmountPaid}
-                         onChange={(e)=> setNewContribution({...newContribution, AmountPaid: e.target.value})}
-                        
-                        />
-                         
-                    </Form.Group>
-                    <Form.Group contrilId="formContrubutionDate">
-                        <Form.Label>Fecha de contribucion</Form.Label>
-                        <Form.Control
-                         type= "date"
-                         placeholder='Ingrese fecha de la contribucion'
-                         value={newContribution.ContributionDate}
-                         onChange={(e)=> setNewContribution({...newContribution, ContributionDate: e.target.value})}
-                        
-                        />
-                         
-                    </Form.Group>
-                </Form>
+
+          {modalAction !== 'detail' && (
+
+            <Form>
+
+              <Form.Group controlId='formMethodId'>
+                <Form.Label><b>Método de Pago</b></Form.Label>
+                <Form.Control
+                  type= "text"
+                  value={newContribution.PaymentMethodId}
+                  onChange={(e)=> setNewContribution({...newContribution, PaymentMethodId: e.target.value})}
+                />
+              </Form.Group>
+
+              <Form.Group controlId='formAmountPaid'>
+                <Form.Label><b>Cantidad</b></Form.Label>
+                <Form.Control
+                  type= "number"
+                  placeholder='Ingrese la cantidad pagada'
+                  value={newContribution.AmountPaid}
+                  onChange={(e)=> setNewContribution({...newContribution, AmountPaid: e.target.value})}
+                />
+              </Form.Group>
+
+              <Form.Group contrilId="formContrubutionDate">
+                <Form.Label><b>Fecha de Contribución</b></Form.Label>
+                <Form.Control
+                  type= "date"
+                  placeholder='Ingrese fecha de la contribucion'
+                  value={newContribution.ContributionDate}
+                  onChange={(e)=> setNewContribution({...newContribution, ContributionDate: e.target.value})}
+                />
+              </Form.Group>
+
+            </Form>
+          )}
+          {modalAction === 'detail' && (
+            <div>
+            {selectedContributionId && (
+              <div>
+                <p><b>Método de Pago:</b> {selectedContributionId}</p>
+                <p><b>Cantidad</b> {newContribution.AmountPaid}</p>
+                <p><b>Fecha de contribucion:</b> {newContribution.ContributionDate}</p>
+              </div>
             )}
-            {modalAction === 'detail' && (
-                <div>
-                    {selectedContributionId && (
-                        <div>
-                            <h4>Detalles de la contribucion</h4>
-                            <p>ID metodo de pago: {selectedContributionId}</p>
-                            <p>Cantidad pagada: {newContribution.AmountPaid}</p>
-                            <p>Fecha de contribucion: {newContribution.ContributionDate}</p>
-                        </div>
-                    )}
-                </div>
-            )}
+            </div>
+          )}
+
         </Modal.Body>
+
         <Modal.Footer>
-            <Button variant='secondary' onClick={handleCloseModal}>
-                Cancelar
+
+          <Button variant='secondary' onClick={handleCloseModal}>
+            Cancelar
+          </Button>
+          
+          {modalAction !== 'detail' && (
+            <Button
+              variant='primary'
+              onClick={modalAction === 'create' ? handleCreateContribution : handleUpdateContribution}
+            >
+              {modalAction === 'create' ? 'Crear' : 'Actualizar'}    
             </Button>
-            {modalAction !== 'detail' && (
-                <Button
-                variant='primary'
-                onClick={modalAction === 'create' ? handleCreateContribution : handleUpdateContribution}
-                >
-                 {modalAction === 'create' ? 'Crear' : 'Actualizar'}    
-                </Button>
-
-
-            )}
+          )}
         </Modal.Footer>
 
-
       </Modal>
-        
-
-    
 
     </div>
-);
+  );
 
 };
 
